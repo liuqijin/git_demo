47efdbd488c72d9afcbd3a87bfed9a0fccf78cf7
liuqijin <1225135470@qq.com>
2024-05-11 21:22:57 +0800

    second

--- src/main/java/com/example/git_demo/GitDemoApplication.java
+++ src/main/java/com/example/git_demo/GitDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitDemoApplication {
-
+//aaaa
     public static void main(String[] args) {
         SpringApplication.run(GitDemoApplication.class, args);
     }
